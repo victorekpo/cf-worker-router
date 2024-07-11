54832900613f7c95782cc4afeb8012e89047d1c4
Vic <victor.ekpo@gmail.com>
2024-07-11 03:31:35 -0500

    add status

--- .husky/precommit.sh
+++ .husky/precommit.sh
@@ -4,6 +4,8 @@ if [ -f "$FILE" ]; then
   sed -i -e 's/account_id = ".*"/account_id = "**********"/' $FILE
   echo "$FILE updated"
   git add -A
+  git status
+  echo "git updated"
 else
   echo "$FILE does not exist."
 fi
