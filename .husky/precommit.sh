5c935d5001cc68a83bcdd5671ecdb429b007e0fb
Vic <victor.ekpo@gmail.com>
2024-07-11 03:19:32 -0500

    update precommit.sh

--- .husky/precommit.sh
+++ .husky/precommit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pwd
-FILE="../wrangler.toml"
+ls
+FILE="./wrangler.toml"
 if [ -f "$FILE" ]; then
   sed -i '' -e 's/account_id = ".*"/account_id = "123**********"/' $FILE
   echo "$FILE updated"
