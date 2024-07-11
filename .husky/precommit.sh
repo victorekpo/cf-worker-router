c3ee158be9a687d6d151d1b74fdde76801e55c03
Vic <victor.ekpo@gmail.com>
2024-07-11 11:07:35 -0500

    update gitignore

--- .husky/precommit.sh
+++ .husky/precommit.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 FILE="./wrangler.toml"
-ASSETS="./assets"
 if [ -f "$FILE" ]; then
   sed -i -e 's/account_id = ".*"/account_id = "**********"/' $FILE
   echo "$FILE updated"
   git add -A
-  rm $ASSETS/*
 else
   echo "$FILE does not exist."
 fi
