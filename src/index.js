7c3b25b9e367eb4e64340303e5635c0d7c8d7f64
Vic <victor.ekpo@gmail.com>
2024-07-11 03:39:42 -0500

    update directory structure

--- src/index.js
+++ src/index.js
@@ -1,75 +1,23 @@
 import { Router } from 'itty-router'
-import { Buffer } from 'buffer'
+import { base64Handler, postHandler, rootHandler } from './routers'
 
 // Create a new router
-const router = Router()
+const router = Router();
+router.get("/", rootHandler);
+router.get("/base64/:text", base64Handler);
+router.post("/post", postHandler);
 
-
-router.get("/", () => {
-  return new Response("Hello, world! This is the root page of your Worker template.")
-})
-
-router.get("/base64/:text", ({ params }) => {
-  // Decode text like "Hello%20world" into "Hello world"
-  let input = decodeURIComponent(params.text)
-
-  // Construct a buffer from our input
-  let buffer = Buffer.from(input, "utf8")
-
-  // Serialise the buffer into a base64 string
-  let base64 = buffer.toString("base64")
-
-  // Return the HTML with the string to the client
-  return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
-    headers: {
-      "Content-Type": "text/html"
-    }
-  })
-})
-
-/*
-This shows a different HTTP method, a POST.
-
-Try send a POST request using curl or another tool.
-
-Try the below curl command to send JSON:
-
-$ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
+/**
+ * This is the last route we define, it will match anything that hasn't hit a route we've defined
+ * above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure
+ * to include it!).
 */
-router.post("/post", async request => {
-  // Create a base object with some fields.
-  let fields = {
-    "asn": request.cf.asn,
-    "colo": request.cf.colo
-  }
+router.all("*", () => new Response("404, not found!", { status: 404 }));
 
-  // If the POST data is JSON then attach it to our response.
-  if (request.headers.get("Content-Type") === "application/json") {
-    fields["json"] = await request.json()
-  }
-
-  // Serialise the JSON to a string.
-  const returnData = JSON.stringify(fields, null, 2);
-
-  return new Response(returnData, {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-})
-
-/*
-This is the last route we define, it will match anything that hasn't hit a route we've defined
-above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
-
-Visit any page that doesn't exist (e.g. /foobar) to see it in action.
-*/
-router.all("*", () => new Response("404, not found!", { status: 404 }))
-
-/*
-This snippet ties our worker to the router we deifned above, all incoming requests
-are passed to the router where your routes are called and the response is sent.
+/**
+ * This snippet ties our worker to the router we defined above, all incoming requests
+ * are passed to the router where your routes are called and the response is sent.
 */
 addEventListener('fetch', (e) => {
   e.respondWith(router.handle(e.request))
-})
+});
---
+++ src/routers/root/router.js
@@ -0,0 +1,3 @@
+export const rootHandler = () => {
+    return new Response("Hello, world! This is the root page of your Worker template.")
+};
