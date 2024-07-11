4a831d56e5cb9289098636dac2bdfb5cb0966ccb
Vic <victor.ekpo@gmail.com>
2024-07-11 03:20:37 -0500

    fix bug in script

--- .husky/precommit.sh
+++ .husky/precommit.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-pwd
-ls
 FILE="./wrangler.toml"
 if [ -f "$FILE" ]; then
-  sed -i -e 's/account_id = ".*"/account_id = "123**********"/' $FILE
+  sed -i -e 's/account_id = ".*"/account_id = "**********"/' $FILE
   echo "$FILE updated"
 else
   echo "$FILE does not exist."
